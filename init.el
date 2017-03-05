dcda0414b16cf97b3f4ac542d1f39dd3cde1fe08
Ting Zhou <zhouting@umich.edu>
2017-03-04 23:27:12 -0500

    Add monokai theme

--- init.el
+++ init.el
@@ -180,6 +180,7 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(solarized-dark
+                         monokai
                          solarized-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
