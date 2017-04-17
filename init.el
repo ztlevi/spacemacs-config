d6eb8f79e63487879e53e595a5b93aae3a42bf76
Ting Zhou <zhouting@umich.edu>
2017-04-17 12:10:29 -0400

    update with latest spacemacs

--- init.el
+++ init.el
@@ -68,7 +68,7 @@ values."
      deft
      markdown
      org
-     shaders
+     gpu
      yaml
      react
      (python :variables
@@ -107,7 +107,7 @@ values."
                     highlight-indentation vi-tilde-fringe eyebrowse
                     org-bullets org-repo-todo org-download org-timer
                     livid-mode git-gutter git-gutter-fringe smooth-scrolling
-                    leuven-theme gh-md evil-lisp-state spray lorem-ipsum
+                    leuven-theme gh-md evil-lisp-state spray lorem-ipsum symon
                     ac-ispell ace-jump-mode auto-complete auto-dictionary
                     clang-format define-word google-translate disaster epic
                     fancy-battery org-present orgit orglue spacemacs-theme
