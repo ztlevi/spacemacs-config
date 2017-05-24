c1f34e43032afabacc9ba2b0a2dbde45f7b158ad
Ting Zhou <zhouting@umich.edu>
2017-05-24 10:16:37 -0400

    Purge org2blog and org-preview

--- layers/ztlevi-org/local/org2blog/metaweblog-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "metaweblog" "0.1.1"
-  "An emacs library to access metaweblog based weblogs"
-  '((xml-rpc "1.6.8")))
--- layers/ztlevi-org/local/org2blog/org2blog-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "org2blog" "20160502.1821" "Blog from Org mode to wordpress"
-  '((org "8.1")
-    (xml-rpc "1.6.8")
-    (metaweblog "0.1")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
