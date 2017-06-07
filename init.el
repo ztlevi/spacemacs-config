3c001f694dcc3b8929e90cbcb1f944e3efae8fb6
Ting Zhou <zhouting@umich.edu>
2017-06-06 23:52:09 -0400

    Add banners

--- init.el
+++ init.el
@@ -162,7 +162,7 @@ values."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner 'official
+   dotspacemacs-startup-banner '"~/.spacemacs.d/icons/banner.png"
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
    ;; Possible values for list-type are:
