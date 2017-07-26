0c1a839854743307969e984a62fa04f590dbbe87
Ting Zhou <zhouting@umich.edu>
2017-07-26 09:57:30 -0400

    Use official banner

--- init.el
+++ init.el
@@ -160,7 +160,7 @@ It should only modify the values of Spacemacs settings."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner '"~/.spacemacs.d/icons/banner.png"
+   dotspacemacs-startup-banner 'official
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
    ;; Possible values for list-type are:
