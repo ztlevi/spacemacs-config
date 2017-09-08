88aa81d730c603b60c160a59b4dfb89bfc7ba095
Ting Zhou <zhouting@umich.edu>
2017-09-07 23:45:07 -0400

    gitignore doom-theme from local
    Remove solarized theme, Update show-paren-match color

--- custom.el
+++ custom.el
@@ -119,6 +119,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(show-paren-match ((t (:background "#da8548" :foreground "#fafafa")))))
+ '(show-paren-match ((t (:background "#Ff69b4" :foreground "#fafafa")))))
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
--- init.el
+++ init.el
@@ -176,8 +176,6 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(doom-one-light
-                         solarized-light
-                         solarized-dark
                          doom-one)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
