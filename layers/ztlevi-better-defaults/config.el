3337d252e8b92f0852503c122b8c2cc9a69d46fa
Ting Zhou <zhouting@umich.edu>
2017-09-15 21:25:29 -0400

    Update electric-pair setting

--- layers/ztlevi-better-defaults/config.el
+++ layers/ztlevi-better-defaults/config.el
@@ -171,9 +171,21 @@ Single Capitals as you type."
 
 (setq tags-add-tables nil)
 
+;; electric-pair setting
 (electric-pair-mode t)
 ;; https://www.reddit.com/r/emacs/comments/4xhxfw/how_to_tune_the_behavior_of_eletricpairmode/
-(setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
+(setq electric-pair-inhibit-predicate 'ztlevi/electric-pair-conservative-inhibit)
+(defun ztlevi/electric-pair-conservative-inhibit (char)
+  (or
+   ;; I find it more often preferable not to pair when the
+   ;; same char is next.
+   (eq char (char-after))
+   ;; Don't pair up when we insert the second of "" or of ((.
+   ;; (and (eq char (char-before))
+   ;;      (eq char (char-before (1- (point)))))
+   ;; I also find it often preferable not to pair next to a word.
+   (eq (char-syntax (following-char)) ?w)))
+
 (show-paren-mode t)
 
 ;; http://oremacs.com/2015/01/17/setting-up-ediff/
