34533e73dce58000b08a034bc3d658236cffee6e
Ting Zhou <zhouting@umich.edu>
2017-09-17 00:49:23 -0400

    Update markdown code face

--- custom.el
+++ custom.el
@@ -118,6 +118,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mmm-default-submode-face ((t (:background "#f0f0f0"))))
+ '(markdown-code-face ((t (:background "#f0f0f0"))))
  '(show-paren-match ((t (:background "#Ff69b4" :foreground "#fafafa")))))
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
