4306285010c99f05981637faa2bd20d33f70f4da
Ting Zhou <zhouting@umich.edu>
2017-11-01 18:08:03 -0400

    Use Menlo

--- init.el
+++ init.el
@@ -205,7 +205,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("DejaVu Sans Mono"
+   dotspacemacs-default-font '("Menlo"
                                :size 18
                                :weight normal
                                :width normal
@@ -510,7 +510,7 @@ It should only modify the values of Spacemacs settings."
   ;;解决org表格里面中英文对齐的问题
   (when (configuration-layer/layer-usedp 'chinese)
     (when (and (spacemacs/system-is-mac) window-system)
-      (spacemacs//set-monospaced-font "Ubuntu Mono" "Hiragino Sans GB" 14 16)))
+      (spacemacs//set-monospaced-font "Menlo" "Hiragino Sans GB" 14 16)))
   
   ;; Setting Chinese Font
   (when (and (spacemacs/system-is-mswindows) window-system)
