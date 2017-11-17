41b1fadb0018029f6041fe5ab98dae8571ee13aa
Ting Zhou <zhouting@umich.edu>
2017-11-17 18:22:28 -0500

    disable global-whitespace-mode

--- layers/ztlevi-ui/packages.el
+++ layers/ztlevi-ui/packages.el
@@ -330,14 +330,6 @@ This segment overrides the modeline functionality of `org-mode-line-string'."
     ;; ;; http://emacsredux.com/blog/2013/05/31/highlight-lines-that-exceed-a-certain-length-limit/
     (setq whitespace-line-column fill-column) ;; limit line length
     ;;https://www.reddit.com/r/emacs/comments/2keh6u/show_tabs_and_trailing_whitespaces_only/
-    (setq whitespace-display-mappings
-          ;; all numbers are Unicode codepoint in decimal. try (insert-char 182 ) to see it
-          '(
-            (space-mark 32 [183] [46])           ; 32 SPACE, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-            (newline-mark 10 [182 10])           ; 10 LINE FEED
-            (tab-mark 9 [187 9] [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
-            ))
-    (setq whitespace-style '(face tabs trailing tab-mark ))
     ;; (setq whitespace-style '(face lines-tail))
     ;; show tab;  use untabify to convert tab to whitespace
     (setq spacemacs-show-trailing-whitespace nil)
@@ -347,7 +339,7 @@ This segment overrides the modeline functionality of `org-mode-line-string'."
     ;; (setq inhibit-eol-conversion t)
     ;; (add-hook 'prog-mode-hook 'whitespace-mode)
 
-    (global-whitespace-mode +1)
+    ;; (global-whitespace-mode +1)
     (add-hook 'before-save-hook 'whitespace-cleanup)
 
     (with-eval-after-load 'whitespace
