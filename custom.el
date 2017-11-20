7d019a2f48fb095b73b5359bd5129420ccbf8c6f
Ting Zhou <zhouting@umich.edu>
2017-11-20 01:18:42 -0500

    set garbage collections threshold

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(+doom-modeline-height 30)
- '(ahs-case-fold-search nil t)
+ '(ahs-case-fold-search nil)
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(command-log-mode-window-size 50)
@@ -26,6 +26,7 @@
  '(expand-region-exclude-text-mode-expansions (quote (html-mode nxml-mode web-mode)))
  '(expand-region-reset-fast-key "r")
  '(flymd-close-buffer-delete-temp-files t)
+ '(gc-cons-threshold 20000000)
  '(global-command-log-mode nil)
  '(helm-buffer-max-length 56)
  '(helm-move-to-line-cycle-in-source t)
