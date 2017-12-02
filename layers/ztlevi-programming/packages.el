68ad27698b991b294ffd3f390fea412580d1e9ee
Ting Zhou <zhouting@umich.edu>
2017-12-01 19:30:15 -0500

    js indent use 2

--- layers/ztlevi-programming/packages.el
+++ layers/ztlevi-programming/packages.el
@@ -381,9 +381,9 @@
         (setq js2-auto-indent-p t)
 
         (setq js2-bounce-indent nil)
-        (setq js-indent-level 4)
-        (setq js2-basic-offset 4)
-        (setq js-switch-indent-offset 4)
+        (setq js-indent-level 2)
+        (setq js2-basic-offset 2)
+        (setq js-switch-indent-offset 2)
         ;; Let flycheck handle parse errors
         (setq js2-mode-show-parse-errors nil)
         (setq js2-mode-show-strict-warnings nil)
