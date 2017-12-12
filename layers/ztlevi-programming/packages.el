476e3f93e3834abdb6f2374f3120e1a966deaad7
Ting Zhou <zhouting@umich.edu>
2017-12-12 01:35:46 -0500

    use C-j instead of <C-i> to jump

--- layers/ztlevi-programming/packages.el
+++ layers/ztlevi-programming/packages.el
@@ -587,7 +587,7 @@
         (bb/define-key company-active-map (kbd "C-f") nil)
         (bb/define-key company-active-map (kbd "<tab>") 'company-complete-selection)
         (bb/define-key company-active-map (kbd "C-w") 'evil-delete-backward-word)
-        (bb/define-key company-active-map (kbd "<C-i>") 'company-show-location)))
+        (bb/define-key company-active-map (kbd "C-j") 'company-show-location)))
     ))
 
 (defun ztlevi-programming/post-init-company-c-headers ()
