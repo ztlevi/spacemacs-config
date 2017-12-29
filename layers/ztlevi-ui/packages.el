ef72b38d395d82ef15121e089a7a8906ba87888c
Ting Zhou <zhouting@umich.edu>
2017-12-29 15:21:28 -0500

    update modeline

--- layers/ztlevi-ui/packages.el
+++ layers/ztlevi-ui/packages.el
@@ -94,22 +94,22 @@
       (concat "TS:" (int-to-string (or mode-indent-level 0)))))
 
   ;; set state tag
-  (setq evil-normal-state-tag    (propertize " 𝗡 " 'face '((:inherit spacemacs-normal-face)))
-        evil-emacs-state-tag     (propertize " 𝗘 " 'face '((:inherit spacemacs-emacs-face)))
-        evil-hybrid-state-tag    (propertize " 𝗛 " 'face '((:inherit spacemacs-hybrid-face)))
-        evil-insert-state-tag    (propertize " 𝗜 " 'face '((:inherit spacemacs-insert-face)))
-        evil-motion-state-tag    (propertize " 𝗠 " 'face '((:inherit spacemacs-motion-face)))
-        evil-visual-state-tag    (propertize " 𝗩 " 'face '((:inherit spacemacs-visual-face)))
-        evil-evilified-state-tag (propertize " 𝗙 " 'face '((:inherit spacemacs-evilified-face)))
-        evil-replace-state-tag   (propertize " 𝗥 " 'face '((:inherit spacemacs-replace-face)))
-        evil-operator-state-tag  (propertize " 𝗢 " 'face '((:background "purple" :foreground "white"))))
+  (setq evil-normal-state-tag    (propertize " N " 'face '((:inherit spacemacs-normal-face)))
+        evil-emacs-state-tag     (propertize " E " 'face '((:inherit spacemacs-emacs-face)))
+        evil-hybrid-state-tag    (propertize " H " 'face '((:inherit spacemacs-hybrid-face)))
+        evil-insert-state-tag    (propertize " I " 'face '((:inherit spacemacs-insert-face)))
+        evil-motion-state-tag    (propertize " M " 'face '((:inherit spacemacs-motion-face)))
+        evil-visual-state-tag    (propertize " V " 'face '((:inherit spacemacs-visual-face)))
+        evil-evilified-state-tag (propertize " F " 'face '((:inherit spacemacs-evilified-face)))
+        evil-replace-state-tag   (propertize " R " 'face '((:inherit spacemacs-replace-face)))
+        evil-operator-state-tag  (propertize " O " 'face '((:background "purple" :foreground "white"))))
 
   (setq my-flycheck-mode-line
         '(:eval
           (pcase flycheck-last-status-change
             ((\` not-checked) nil)
             ((\` no-checker) (propertize " -" 'face 'warning))
-            ((\` running) (propertize " ✷" 'face 'success))
+            ((\` running) (propertize " *" 'face 'success))
             ((\` errored) (propertize " !" 'face 'error))
             ((\` finished)
              (let* ((error-counts (flycheck-count-errors flycheck-current-errors))
@@ -126,16 +126,16 @@
   (setq-default mode-line-misc-info
                 (assq-delete-all 'which-func-mode mode-line-misc-info))
 
-  (setq modeline-height 1.35)
+  (setq modeline-height 1.25)
   (setq-default mode-line-format
                 (list
                  ;; evil state
                  '(:eval evil-mode-line-tag)
 
                  ;; set the modeline height
-                 '(:eval (propertize
+                 `(:eval (propertize
                           " %1"
-                          'face '(:height `modeline-height)))
+                          'face '(:height ,modeline-height)))
 
                  ;; linum
                  ;; '(:eval (propertize
@@ -160,24 +160,19 @@
                  ;;                               "insert") " mode")))
 
                  ;; was this buffer modified since the last save?
-                 '(:eval (if (buffer-modified-p)
-                             (propertize
-                              "M"
-                              'face '(:inherit font-lock-string-face
-                                               :weight bold :underline t :overline t)
-                              'help-echo "Buffer has been modified")
-                           " "))
-
-                 " "
-
-                 ;; is this buffer read-only?
-                 '(:eval (if buffer-read-only
-                             (propertize
-                              "R"
-                              'face '(:inherit font-lock-type-face
-                                               :weight bold :underline t :overline t)
-                              'help-echo "Buffer is read-only")
-                           " "))
+                 '(:eval
+                   (if (not (and (buffer-modified-p) buffer-read-only))
+                       (cond ((buffer-modified-p)
+                              (propertize
+                               "Mod"
+                               'face '(:inherit font-lock-preprocessor-face)
+                               'help-echo "Buffer has been modified"))
+                             (buffer-read-only
+                              (propertize
+                               "RO"
+                               'face '(:inherit font-lock-type-face :weight bold)
+                               'help-echo "Buffer is read-only")))
+                     ))
 
                  " "
                  ;; anzu
