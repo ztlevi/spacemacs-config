f037599bc9d91ebc20427d9b0d5b4116fe587c9c
Ting Zhou <zhouting@umich.edu>
2018-01-24 20:24:13 -0500

    update ztlevi/evil-quick-replace

--- layers/ztlevi-misc/funcs.el
+++ layers/ztlevi-misc/funcs.el
@@ -420,7 +420,7 @@ e.g. Sunday, September 17, 2000."
   (when (evil-visual-state-p)
     (evil-exit-visual-state)
     (let ((selection (regexp-quote (buffer-substring-no-properties beg end))))
-      (setq command-string (format "%%s /%s//g" selection))
+      (setq command-string (format "1,$s /%s//g" selection))
       (minibuffer-with-setup-hook
           (lambda () (backward-char 2))
         (evil-ex command-string)))))
