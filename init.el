7f49076ed2b7ea83e9c659da3f47cd517ee76095
Ting Zhou <zhouting@umich.edu>
2018-01-31 12:53:36 -0500

    disable colorize cursor according to state

--- init.el
+++ init.el
@@ -264,7 +264,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    ;; (default t)
-   dotspacemacs-colorize-cursor-according-to-state t
+   dotspacemacs-colorize-cursor-according-to-state nil
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
