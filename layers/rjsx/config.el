51549dd445d527fc303b74af0b4208e57872b46e
Ting Zhou <zhouting@umich.edu>
2018-03-20 18:32:18 -0700

    comment jsx region with S/

--- layers/rjsx/config.el
+++ layers/rjsx/config.el
@@ -1,5 +1,9 @@
 (spacemacs|define-jump-handlers rjsx-mode)
 
+;; comment jsx region
+(add-hook 'rjsx-mode-hook (lambda ()
+                            (push '(?/ . ("{/*" . "*/}")) evil-surround-pairs-alist)))
+
 ;; (add-to-list 'auto-mode-alist '("\\.react.js\\'" . rjsx-mode))
 ;; (add-to-list 'auto-mode-alist '("\\index.android.js\\'" . rjsx-mode))
 ;; (add-to-list 'auto-mode-alist '("\\index.ios.js\\'" . rjsx-mode))
