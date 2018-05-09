4ccb9291c502bf66ea1158f368e6221b15af1c6b
Ting Zhou <zhouting@umich.edu>
2018-05-08 17:54:06 -0700

    Add: use company-box and remove company-childframe

--- layers/ztlevi-ui/packages.el
+++ layers/ztlevi-ui/packages.el
@@ -19,7 +19,9 @@
     ;; (ztlevi-modeline :location local)
     popwin
     (ivy-posframe :toggle (version<= "26" emacs-version))
-    (company-childframe :toggle (version<= "26" emacs-version))
+    ;; company-box needs icons-in-terminal
+    (icons-in-terminal :location local)
+    company-box
     (whitespace :location built-in)
     doom-themes
     ;; To use local repo, update the packages to clean up the cache
@@ -96,11 +98,56 @@
 
     (ivy-posframe-enable)))
 
-(defun ztlevi-ui/init-company-childframe ()
-  (use-package company-childframe
+(defun ztlevi-ui/init-icons-in-terminal ()
+  (use-package icons-in-terminal
+    :defer t))
+
+(defun ztlevi-ui/init-company-box ()
+  (use-package company-box
     :defer t
-    :init
-    (company-childframe-mode 1)))
+    :hook (company-mode . company-box-mode)
+    :config
+    (require 'icons-in-terminal)
+    (progn
+      (setq company-box-backends-colors nil)
+
+      (setq company-box-icons-unknown 'fa_question_circle)
+
+      (setq company-box-icons-elisp
+            '((fa_tag :face font-lock-function-name-face) ;; Function
+              (fa_cog :face font-lock-variable-name-face) ;; Variable
+              (fa_cube :face font-lock-constant-face)     ;; Feature
+              (md_color_lens :face font-lock-doc-face)))  ;; Face
+
+      (setq company-box-icons-yasnippet 'fa_bookmark)
+
+      (setq company-box-icons-lsp
+            '((1 . fa_text_height)                               ;; Text
+              (2 . (fa_tags :face font-lock-function-name-face)) ;; Method
+              (3 . (fa_tag :face font-lock-function-name-face))  ;; Function
+              (4 . (fa_tag :face font-lock-function-name-face))  ;; Constructor
+              (5 . (fa_cog :foreground "#FF9800"))               ;; Field
+              (6 . (fa_cog :foreground "#FF9800"))               ;; Variable
+              (7 . (fa_cube :foreground "#7C4DFF"))              ;; Class
+              (8 . (fa_cube :foreground "#7C4DFF"))              ;; Interface
+              (9 . (fa_cube :foreground "#7C4DFF"))              ;; Module
+              (10 . (fa_cog :foreground "#FF9800"))              ;; Property
+              (11 . md_settings_system_daydream)                 ;; Unit
+              (12 . (fa_cog :foreground "#FF9800"))              ;; Value
+              (13 . (md_storage :face font-lock-type-face))      ;; Enum
+              (14 . (md_closed_caption :foreground "#009688"))   ;; Keyword
+              (15 . md_closed_caption)                           ;; Snippet
+              (16 . (md_color_lens :face font-lock-doc-face))    ;; Color
+              (17 . fa_file_text_o)                              ;; File
+              (18 . md_refresh)                                  ;; Reference
+              (19 . fa_folder_open)                              ;; Folder
+              (20 . (md_closed_caption :foreground "#009688"))   ;; EnumMember
+              (21 . (fa_square :face font-lock-constant-face))   ;; Constant
+              (22 . (fa_cube :face font-lock-type-face))         ;; Struct
+              (23 . fa_calendar)                                 ;; Event
+              (24 . fa_square_o)                                 ;; Operator
+              (25 . fa_arrows)) ;; TypeParameter
+            ))))
 
 (defun ztlevi-ui/post-init-pangu-spacing ()
   (progn
