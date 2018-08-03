b8bcc5ab783c8a5ade1f424e031ece7672b27ac2
Ting Zhou <zhouting@umich.edu>
2018-08-03 10:30:12 -0700

    update ztlevi/capture-screenshot, use local img folder

--- layers/ztlevi-org/funcs.el
+++ layers/ztlevi-org/funcs.el
@@ -63,7 +63,7 @@
       (setq basename (format-time-string "%Y%m%d_%H%M%S")))
   (setq fullpath
         (concat (file-name-directory (buffer-file-name))
-                "../img/"
+                "img/"
                 (file-name-base (buffer-file-name))
                 "_"
                 basename))
@@ -72,18 +72,16 @@
                 "_"
                 basename
                 ".png"))
-  (if (file-exists-p (file-name-directory fullpath))
-      (progn
-        (setq final-image-full-path (concat fullpath ".png"))
-        (call-process "screencapture" nil nil nil "-s" final-image-full-path)
-        (if (executable-find "convert")
-            (progn
-              (setq resize-command-str (format "convert %s -resize 800x600 %s" final-image-full-path final-image-full-path))
-              (shell-command-to-string resize-command-str)))
-        (ztlevi//insert-org-or-md-img-link "../img/" relativepath))
-    (progn
-      (call-process "screencapture" nil nil nil "-s" (concat basename ".png"))
-      (ztlevi//insert-org-or-md-img-link "./" (concat basename ".png"))))
+  (unless (file-exists-p (file-name-directory fullpath))
+      (make-directory "img"))
+  (progn
+    (setq final-image-full-path (concat fullpath ".png"))
+    (call-process "screencapture" nil nil nil "-s" final-image-full-path)
+    (if (executable-find "convert")
+        (progn
+          (setq resize-command-str (format "convert %s -resize 800x600 %s" final-image-full-path final-image-full-path))
+          (shell-command-to-string resize-command-str)))
+    (ztlevi//insert-org-or-md-img-link "./img/" relativepath))
   (insert "\n"))
 
 (defun ztlevi/org-archive-done-tasks ()
