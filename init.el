6b4a0bd1610454738b574a361f41dc1ef06e67a6
Ting Zhou <zhouting@umich.edu>
2018-08-03 10:02:07 -0700

    disable line-numbers

--- init.el
+++ init.el
@@ -514,15 +514,7 @@ It should only modify the values of Spacemacs settings."
    ;;                       text-mode
    ;;   :size-limit-kb 1000)
    ;; (default nil)
-   dotspacemacs-line-numbers '(:relative
-                               nil
-                               :disabled-for-modes dired-mode
-                               doc-view-mode
-                               markdown-mode
-                               org-mode
-                               pdf-view-mode
-                               text-mode
-                               :size-limit-kb 1000)
+   dotspacemacs-line-numbers nil
 
    ;; Code folding method. Possible values are `evil' and `origami'.
    ;; (default 'evil)
