55d198b59e476b348ec6d5ffc8a6d82069070923
Ting Zhou <zhouting@umich.edu>
2018-09-14 09:57:57 -0700

    update company-box

--- layers/ztlevi-ui/packages.el
+++ layers/ztlevi-ui/packages.el
@@ -103,43 +103,41 @@
     :config
     (require 'icons-in-terminal)
     (progn
-      (setq company-box-backends-colors nil)
+      (setq company-box-backends-colors nil
+            company-box-max-candidates 50)
 
       (setq company-box-icons-unknown 'fa_question_circle)
-
       (setq company-box-icons-elisp
             '((fa_tag :face font-lock-function-name-face) ;; Function
               (fa_cog :face font-lock-variable-name-face) ;; Variable
-              (fa_cube :face font-lock-constant-face)     ;; Feature
-              (md_color_lens :face font-lock-doc-face)))  ;; Face
-
+              (fa_cube :face font-lock-constant-face) ;; Feature
+              (md_color_lens :face font-lock-doc-face))) ;; Face
       (setq company-box-icons-yasnippet 'fa_bookmark)
-
       (setq company-box-icons-lsp
-            '((1 . fa_text_height)                               ;; Text
+            '((1 . fa_text_height) ;; Text
               (2 . (fa_tags :face font-lock-function-name-face)) ;; Method
-              (3 . (fa_tag :face font-lock-function-name-face))  ;; Function
-              (4 . (fa_tag :face font-lock-function-name-face))  ;; Constructor
-              (5 . (fa_cog :foreground "#FF9800"))               ;; Field
-              (6 . (fa_cog :foreground "#FF9800"))               ;; Variable
-              (7 . (fa_cube :foreground "#7C4DFF"))              ;; Class
-              (8 . (fa_cube :foreground "#7C4DFF"))              ;; Interface
-              (9 . (fa_cube :foreground "#7C4DFF"))              ;; Module
-              (10 . (fa_cog :foreground "#FF9800"))              ;; Property
-              (11 . md_settings_system_daydream)                 ;; Unit
-              (12 . (fa_cog :foreground "#FF9800"))              ;; Value
-              (13 . (md_storage :face font-lock-type-face))      ;; Enum
-              (14 . (md_closed_caption :foreground "#009688"))   ;; Keyword
-              (15 . md_closed_caption)                           ;; Snippet
-              (16 . (md_color_lens :face font-lock-doc-face))    ;; Color
-              (17 . fa_file_text_o)                              ;; File
-              (18 . md_refresh)                                  ;; Reference
-              (19 . fa_folder_open)                              ;; Folder
-              (20 . (md_closed_caption :foreground "#009688"))   ;; EnumMember
-              (21 . (fa_square :face font-lock-constant-face))   ;; Constant
-              (22 . (fa_cube :face font-lock-type-face))         ;; Struct
-              (23 . fa_calendar)                                 ;; Event
-              (24 . fa_square_o)                                 ;; Operator
+              (3 . (fa_tag :face font-lock-function-name-face)) ;; Function
+              (4 . (fa_tag :face font-lock-function-name-face)) ;; Constructor
+              (5 . (fa_cog :foreground "#FF9800")) ;; Field
+              (6 . (fa_cog :foreground "#FF9800")) ;; Variable
+              (7 . (fa_cube :foreground "#7C4DFF")) ;; Class
+              (8 . (fa_cube :foreground "#7C4DFF")) ;; Interface
+              (9 . (fa_cube :foreground "#7C4DFF")) ;; Module
+              (10 . (fa_cog :foreground "#FF9800")) ;; Property
+              (11 . md_settings_system_daydream) ;; Unit
+              (12 . (fa_cog :foreground "#FF9800")) ;; Value
+              (13 . (md_storage :face font-lock-type-face)) ;; Enum
+              (14 . (md_closed_caption :foreground "#009688")) ;; Keyword
+              (15 . md_closed_caption) ;; Snippet
+              (16 . (md_color_lens :face font-lock-doc-face)) ;; Color
+              (17 . fa_file_text_o) ;; File
+              (18 . md_refresh) ;; Reference
+              (19 . fa_folder_open) ;; Folder
+              (20 . (md_closed_caption :foreground "#009688")) ;; EnumMember
+              (21 . (fa_square :face font-lock-constant-face)) ;; Constant
+              (22 . (fa_cube :face font-lock-type-face)) ;; Struct
+              (23 . fa_calendar) ;; Event
+              (24 . fa_square_o) ;; Operator
               (25 . fa_arrows)) ;; TypeParameter
             ))))
 
