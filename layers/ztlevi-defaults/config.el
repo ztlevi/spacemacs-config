403665f203ce1bbcf96040698e0a212dca53fc47
Ting Zhou <zhouting@umich.edu>
2018-09-30 20:14:48 -0700

    remove auto-format-region-code

--- layers/ztlevi-defaults/config.el
+++ layers/ztlevi-defaults/config.el
@@ -42,29 +42,6 @@
 ;; delete the selection with a key press
 (delete-selection-mode t)
 
-;;add auto format paste code
-(dolist (command '(yank yank-pop))
-  (eval
-   `(defadvice ,command (after indent-region activate)
-      (and (not current-prefix-arg)
-           (member major-mode
-                   '(emacs-lisp-mode
-                     lisp-mode
-                     clojure-mode
-                     scheme-mode
-                     haskell-mode
-                     ruby-mode
-                     rspec-mode
-                     python-mode
-                     c-mode
-                     c++-mode
-                     objc-mode
-                     latex-mode
-                     js-mode
-                     plain-tex-mode))
-           (let ((mark-even-if-inactive transient-mark-mode))
-             (indent-region (region-beginning) (region-end) nil))))))
-
 ;; tramp, for sudo access
 ;; very slow!!!!
 ;; for profiling emacs --debug-init --timed-requires --profile
